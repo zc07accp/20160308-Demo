f3cf2839d584bf00a2132be7c5b41d836adb8f65
zf <zf@zfdemacbook-pro.local>
2016-03-08 22:17:11 +0800

    Initial Commit

---
+++ 20160308登陆Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  20160308登陆Demo
+//
+//  Created by zf on 16/3/8.
+//  Copyright © 2016年 PY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 20160308登陆Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  20160308登陆Demo
+//
+//  Created by zf on 16/3/8.
+//  Copyright © 2016年 PY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
